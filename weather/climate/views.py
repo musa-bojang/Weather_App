d45f2c795eb00d94a2caa07862f4d1f6b2099622
Digital Marketing Template <musabojang413@gmail.com>
2020-07-29 02:05:18 +0800

    Last Changes to the file

--- weather/climate/views.py
+++ weather/climate/views.py
@@ -1,12 +1,19 @@
 import requests
 from django.shortcuts import render
 from .models import City
+from .forms import CityForm
 # Create your views here.
 def index(request):
     # change units=imperial to units=metric
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=8e9fa2323ae693217ad94ed7f6647945'
-    city = 'Nilai'
+    # city = 'Nilai'
     cities = City.objects.all()
+
+    if request.method =='POST':
+        form = CityForm(request.POST)
+        form.save()
+        pass 
+    form = CityForm()
     # print(r.text)
     weather_data =[]
     
@@ -21,5 +28,5 @@ def index(request):
         }
         weather_data.append(city_weather)
     # print(weather_data)
-    context = {'weather_data': weather_data}
+    context = {'weather_data': weather_data, 'form': form}
     return render (request, 'climate/weather.html', context)
