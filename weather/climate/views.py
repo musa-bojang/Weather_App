bf1947046edf9127b13536a70a82af53664cccb2
Digital Marketing Template <musabojang413@gmail.com>
2020-07-28 16:40:51 +0800

    install of pipenv and requests library

--- weather/climate/views.py
+++ weather/climate/views.py
@@ -1,5 +1,18 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render (request, 'climate/weather.html')
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=8e9fa2323ae693217ad94ed7f6647945'
+    city = 'Las Vegas'
+    r = requests.get(url.format(city)).json()
+    # print(r.text)
+
+    city_weather ={
+        'city': city,
+         'temperature': r['main']['temp'],
+         'description':r['weather'][0]['description'],
+         'icon':r['weather'][0]['icon'] ,
+    }
+    context = {'city_weather': city_weather}
+    return render (request, 'climate/weather.html', context)
